ed32dae6227ca610f8602082435f949a4250f9a9
Jérémy VIGNELLES <jeremy.vignelles@dev3i.fr>
2019-12-11 11:47:42 +0100

    Repro for the msbuild warning in RX

---
+++ TestNewWPF/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace TestNewWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello, World!");
+        }
+    }
+}
